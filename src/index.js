889eea43283403fc12e59812acd65bace242715d
nikhil-k-v <nkvijay08@gmail.com>
2022-06-08 20:31:19 -0400

    controls

--- src/index.js
+++ src/index.js
@@ -2,21 +2,52 @@ import { createRoot } from 'react-dom/client'
 import React, { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, Stars } from "@react-three/drei";
-import { Physics, usePlane, useBox } from "@react-three/cannon";
+import { Physics, usePlane, useBox, useSphere } from "@react-three/cannon";
 import "./index.css";
 
 
 function Box() {
-  const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
+  const [ref, api] = useSphere(() => ({ mass: 1, position: [0, 2, 0] }));
+
+  function handleKeyPress(e){
+    console.log("bye")
+  }
+
+
+async function init() {
+  window.addEventListener( 'keydown', function(e){
+    if(e.key === "w"){
+      console.log("man")
+      api.velocity.set(0,0,10);
+    }
+    if(e.key === "s"){
+      api.velocity.set(0,0,-10);
+    }
+    if(e.key === "d"){
+      api.velocity.set(-10,0,0);
+    }
+    if(e.key === "a"){
+      api.velocity.set(10,0,0);
+    }
+    if(e.key === " "){
+      api.velocity.set(0,5,0);
+    }
+  })
+  window.addEventListener( 'keyup', function(e){
+    api.velocity.set(0,0,0);
+  })
+}
+
+init();
+
   return (
     <mesh
-      onClick={() => {
-        api.velocity.set(0, 2, 0);
-      }}
+      onKeyPress={(e) => handleKeyPress(e)}
+      onClick={() => console.log("hello")}
       ref={ref}
-      position={[0, 2, 0]}
+      position={[0, 5, 0]}
     >
-      <boxBufferGeometry attach="geometry" />
+      <sphereBufferGeometry attach="geometry" />
       <meshLambertMaterial attach="material" color="hotpink" />
     </mesh>
   );
@@ -41,7 +72,7 @@ createRoot(document.getElementById('root')).render(
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 15, 10]} angle={0.3} />
       <Physics>
-        <Box />
+        <Box /> 
         <Plane />
       </Physics>
     </Canvas>,
